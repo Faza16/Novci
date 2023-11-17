f1a5e4dfd4f586c5eb8fbc1d5ac4d7af9d3e3a0b
Zjuti <vedad.kozica.2006@gmail.com>
2023-11-17 01:31:05 +0100

    Fje za određivanje koje karte su u ruci igraca(nisam siguran za klasu da li ce da radi)

--- zadaci/PokerStandard/fje_poker.cpp
+++ zadaci/PokerStandard/fje_poker.cpp
@@ -4,17 +4,23 @@ using namespace std;
 
     class Karte1 {
         private:
-        char znakKarte [2];
-        int brojKarte [2];
+            char znakKarte [5];
+            int brojKarte [5];
 
         public:
-        cin>>znakKarte[0];
-        cin>>brojKarte[0];
-        cin>>znakKarte[1];
-        cin>>brojKarte[1];
+            znakKarte[0] = znakKarte();
+            brojKarte[0] = unosBrojaKarte();
+            znakKarte[0] = znakKarte();
+            brojKarte[1] = unosBrojaKarte();
+            znakKarte[2] = znakKarte();
+            brojKarte[2] = unosBrojaKarte();
+            znakKarte[3] = znakKarte();
+            brojKarte[3] = unosBrojaKarte();
+            znakKarte[4] = znakKarte();
+            brojKarte[4] = unosBrojaKarte();
     }
 
-    int brojIgraca(int a){
+    int brojIgraca(){
         int igraci;
         bool jedanIgrac = true;
         while(jedanIgrac){
@@ -27,4 +33,36 @@ using namespace std;
             }
         }
         return igraci;
-    } 
+    }
+
+    int unosBrojaKarte(){
+        int brojKarte;
+        bool dobarBroj = true;
+        while(dobarBroj){
+            cin>>brojKarte;
+            if (brojKarte>=1 && brojKarte<=14 && brojKarte!=11){
+                dobarBroj = false;
+            }
+            else{
+                cout<<"Ovaj broj ne može biti na karti"<<endl;
+            }
+        }
+        return brojKarte;
+    }
+
+    char unosZnakaKarte(){
+        char znakKarte;
+        bool dobarZnak = true;
+        while(dobarZnak){
+            cin>>znakKarte;
+            if (znakKarte!='c' && znakKarte!='d' && znakKarte!='h' && znakKarte!='s'){
+                dobarBroj = false;
+            }
+            else{
+                cout<<"Ovaj znak ne može biti na karti"<<endl;
+            }
+        }
+        return znakKarte;
+    }
+
+
