a8e4a055f18e5182ae7cc75642e766fa631a4f63
Zjuti <vedad.kozica.2006@gmail.com>
2023-11-17 13:52:16 +0100

    Pocetak rada u poker cpp-u

---
+++ zadaci/PokerStandard/poker.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+#include "fje_poker.cpp"
+
+using namespace std;
+
+class Karte1 {
+        private:
+            char znakKarte [5];
+            int brojKarte [5];
+
+        public:
+            znakKarte[0] = znakKarte();
+            brojKarte[0] = unosBrojaKarte();
+            znakKarte[0] = znakKarte();
+            brojKarte[1] = unosBrojaKarte();
+            znakKarte[2] = znakKarte();
+            brojKarte[2] = unosBrojaKarte();
+            znakKarte[3] = znakKarte();
+            brojKarte[3] = unosBrojaKarte();
+            znakKarte[4] = znakKarte();
+            brojKarte[4] = unosBrojaKarte();
+    };
+
+int main(){
+    int igraci = brojIgraca();
+
+}
