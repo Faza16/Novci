23ec7864514a55d2e51e37380729cf46751891ff
Zjuti <vedad.kozica.2006@gmail.com>
2023-11-17 14:35:16 +0100

    Ugasio klasu

--- zadaci/PokerStandard/poker.cpp
+++ zadaci/PokerStandard/poker.cpp
@@ -3,24 +3,6 @@
 
 using namespace std;
 
-class Karte1 {
-        private:
-            char znakKarte [5];
-            int brojKarte [5];
-
-        public:
-            znakKarte[0] = unosZnakaKarte();
-            brojKarte[0] = unosBrojaKarte();
-            znakKarte[0] = unosZnakaKarte();
-            brojKarte[1] = unosBrojaKarte();
-            znakKarte[2] = unosZnakaKarte();
-            brojKarte[2] = unosBrojaKarte();
-            znakKarte[3] = unosZnakaKarte();
-            brojKarte[3] = unosBrojaKarte();
-            znakKarte[4] = unosZnakaKarte();
-            brojKarte[4] = unosBrojaKarte();
-    }; // treba vidjeti kako se ovo puni
-
 int main(){
     int igraci = brojIgraca();
     char znakKarte [5];
