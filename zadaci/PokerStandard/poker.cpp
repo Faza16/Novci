e1426c9ac0dc4391e1a43d5954702035f21c889b
Zjuti <vedad.kozica.2006@gmail.com>
2023-11-18 23:12:24 +0100

    Radi sad

--- zadaci/PokerStandard/poker.cpp
+++ zadaci/PokerStandard/poker.cpp
@@ -6,26 +6,26 @@ using namespace std;
 int main(){
     cout<<"Unesite broj igrača: ";
     int igraci = brojIgraca();
-    bool ispravnaRuka;
+    bool ispravnaRuka = true;
     char znakKarte [5];
     int brojKarte [5];
     int score[10]; //prvi ide za koja je kombinacija, ostali za poredenja ostalih (nije nuzno 10, vjrv je manje)
-    cout<<"Unesite svoje karte."<<endl;
+    cout<<"Unosite svoje karte."<<endl;
 
     while(ispravnaRuka){
         for(int i=0; i<=4; i++){
-            cout<<"Unesite znak "<< i+1 <<". karte"<<endl;
+            cout<<"Unesite znak "<< i+1 <<". karte:"<<endl;
             znakKarte[i] = unosZnakaKarte();
-            cout<<"Unesite broj "<< i+1 <<". karte"<<endl;
+            cout<<"Unesite broj "<< i+1 <<". karte:"<<endl;
             brojKarte[i] = unosBrojaKarte();
         }
+        cout<<endl;
 
         for(int j=0; j<=4; j++){
-            if((znakKarte[j]==znakKarte[0] && brojKarte[j]==brojKarte[0]) ||
-               (znakKarte[j]==znakKarte[1] && brojKarte[j]==brojKarte[1]) ||
-               (znakKarte[j]==znakKarte[2] && brojKarte[j]==brojKarte[2]) ||
-               (znakKarte[j]==znakKarte[3] && brojKarte[j]==brojKarte[3]) ||
-               (znakKarte[j]==znakKarte[4] && brojKarte[j]==brojKarte[4]) ){
+            if((znakKarte[j]==znakKarte[j-1] && brojKarte[j]==brojKarte[j-1]) ||
+               (znakKarte[j]==znakKarte[j-2] && brojKarte[j]==brojKarte[j-2]) ||
+               (znakKarte[j]==znakKarte[j-3] && brojKarte[j]==brojKarte[j-3]) ||
+               (znakKarte[j]==znakKarte[j-4] && brojKarte[j]==brojKarte[j-4]) ){
                 cout<<"Nije moguće imati dvije iste karte u ruci, unesite ponovo karte!"<<endl;
                 break;
             }
